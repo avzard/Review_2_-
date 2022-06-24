420866570fa5b216607c1b0f4a69b1ffc2d1833b
Abhishek Jadhav <59304331+avzard@users.noreply.github.com>
2022-06-24 11:07:11 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Program
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            EmpPresentAbsent prg = new EmpPresentAbsent();
+            prg.CheckPartTimeEmpWage();
+        }
+    }
+}
